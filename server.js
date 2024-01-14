f62828d6517812af283021efe9dd00fa96161310
aman21376 <108428995+aman21376@users.noreply.github.com>
2024-01-15 01:00:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
+import path from 'path';
+import { fileURLPath } from 'url';
 
 //configure env
 dotenv.config();
@@ -14,6 +16,8 @@ dotenv.config();
 //databse config
 connectDB();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname= path.dirname(__filename);
 //rest object
 const app = express();
 
@@ -21,11 +25,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-
+app.use(express.static(path.join(__dirname,"./client/build")));
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
+
 
 //rest api
 app.get("/", (req, res) => {
