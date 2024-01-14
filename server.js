da8ade2ba55df9856b631a50ee050a765dfba9ab
aman21376 <108428995+aman21376@users.noreply.github.com>
2024-01-15 01:08:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -33,9 +33,9 @@ app.use("/api/v1/product", productRoutes);
 
 
 //rest api
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to ecommerce app</h1>");
-});
+app.use('*',function(req,res){
+  res.sendFile(path.join(__dirname,"./client/build/index.html"))
+})
 
 //PORT
 const PORT = process.env.PORT || 8080;
